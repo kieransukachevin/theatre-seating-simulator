a0bc18358b62fbbae089f4055d2ab989b35a8b9f
Kieran Sukachevin <kieran@kierans-mbp-2.lan>
2020-10-21 17:30:06 -0400

    spacing errors corrected

--- theatre_model.py
+++ theatre_model.py
@@ -1,18 +1,18 @@
 class TheatreModel:
-    def __init__(self, num_rows, num_cols, price):
-    	'''Initialize the model.'''
+	def __init__(self, num_rows, num_cols, price):
+		'''Initialize the model.'''
 
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.price = price
-        self.total_revenue = 0
-        self.seats = {}
-        self.rows = {}
-        self.cols = {}
+		self.num_rows = num_rows
+		self.num_cols = num_cols
+		self.price = price
+		self.total_revenue = 0
+		self.seats = {}
+		self.rows = {}
+		self.cols = {}
 
 class Seat:
-    def __init__(self, state, price):
-    	'''Initialize a seat in the model.'''
+	def __init__(self, state, price):
+		'''Initialize a seat in the model.'''
 
-        self.state = state
-        self.price = price
+		self.state = state
+		self.price = price
